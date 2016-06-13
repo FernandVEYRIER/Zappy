01eac488754862a23ac1b889c9fc344049b70c51
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2016-06-14 01:45:47 +0200

    Settings Menu

--- Zappy/Assets/Scripts/CanvasManager.cs
+++ Zappy/Assets/Scripts/CanvasManager.cs
@@ -4,10 +4,14 @@ using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour {
 
-
+    private GameObject open = null;
     public void Show(GameObject obj)
     {
         obj.SetActive(!obj.activeSelf);
+        if (obj.activeSelf)
+            open = obj;
+        else
+            open = null;
     }
 
     public void FlipImage(RectTransform source)
@@ -18,5 +22,24 @@ public class CanvasManager : MonoBehaviour {
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void ShowIf(GameObject obj, int v1, int v2)
+    {
+        obj.SetActive(v1 == v2);
+    }
+
+    public void Mute()
+    {
+        Camera.main.GetComponent<AudioListener>().enabled = !Camera.main.GetComponent<AudioListener>().enabled;
+    }
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Escape) ||  Input.GetAxis("Cancel") != 0) && open != null)
+        {
+            open.SetActive(false);
+            open = null;
+        }
     }
 }
---
+++ Zappy/Assets/SliderUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SliderUI : MonoBehaviour {
+
+    public GameObject obj;
+    public void Show(Slider slider)
+    {
+        obj.SetActive(slider.maxValue == slider.value);
+    }
+}
