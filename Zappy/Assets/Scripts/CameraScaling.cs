61c10e1d3e37e9cbaa89d9e1907aeb1c9ea249af
Nicolas-Constanty <nicolas.constanty@epitech.eu>
2016-06-23 07:19:41 +0200

    AdjustCam

--- Zappy/Assets/Scripts/CameraScaling.cs
+++ Zappy/Assets/Scripts/CameraScaling.cs
@@ -9,21 +9,22 @@ public class CameraScaling : MonoBehaviour {
 	private Vector3[] dirs = new Vector3[4];
 	private int count = 0;
     private InfinitTerrain terrain;
-
+    private bool init = false;
+    private float offsetX;
 	void Awake()
 	{
 		cam = Camera.main;
-
-		dirs [0] = new Vector3 (0, 0, 0);
-		dirs [1] = new Vector3 (Screen.width, 0, 0);
-		dirs [2] = new Vector3 (0, Screen.height, 0);
-		dirs [3] = new Vector3 (Screen.width, Screen.height, 0);
+        offsetX = 50;
+        dirs [0] = new Vector3 (-offsetX, 0, 0);
+		dirs [1] = new Vector3 (Screen.width + offsetX, 0, 0);
+		dirs [2] = new Vector3 (-offsetX, Screen.height, 0);
+		dirs [3] = new Vector3 (Screen.width + offsetX, Screen.height, 0);
         terrain = GameObject.FindGameObjectWithTag("Terrain").GetComponent<InfinitTerrain>();
 	}
 
 	void Update ()
 	{
-        if (terrain.status)
+        if (terrain.status && !init)
         {
             for (int i = 0; count != 4 && i < 4; ++i)
             {
@@ -36,9 +37,8 @@ public class CameraScaling : MonoBehaviour {
             if (count != 4)
             {
                 count = 0;
-                cam.transform.Translate(Vector3.forward * 0.05f);
+                cam.transform.Translate(Vector3.forward * 0.5f);
             }
         }
-		
 	}
 }
